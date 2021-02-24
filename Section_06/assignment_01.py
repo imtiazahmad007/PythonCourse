3daf38778dad8576e7312b4570cdc92ba6a13a9b
Imtiaz Ahmad <imtiazahmad007@gmail.com>
2021-02-23 19:01:18 -0500

    Add files via upload

--- Section_06/assignment_01.py
+++ Section_06/assignment_01.py
@@ -12,21 +12,68 @@ words_to_aggregate = sys.argv[2:]
 # Expected Output:
 # {"there": n, "Michael": n, "running": n}
 
-words_and_counts = {}
-
-for word in words_to_aggregate:
-    count = 0
-    for path, folder_names, file_names in os.walk(directory_containing_files):
-        for file_name in file_names:
-            file = os.path.join(path, file_name)
-            with open(file, "r") as a_file:
-                for line in a_file:
-                    if re.search(word, line):
-                        word_list = re.findall(word, line)
-                        count += len(word_list)
-
-    words_and_counts[word] = count
+# Your Code Below:
 
 
 
-print(words_and_counts)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Solution:
+# words_and_counts = {}
+#
+# for word in words_to_aggregate:
+#     count = 0
+#     for path, folder_names, file_names in os.walk(directory_containing_files):
+#         for file_name in file_names:
+#             file = os.path.join(path, file_name)
+#             with open(file, "r") as a_file:
+#                 for line in a_file:
+#                     if re.search(word, line):
+#                         word_list = re.findall(word, line)
+#                         count += len(word_list)
+#
+#     words_and_counts[word] = count
+#
+#
+#
+# print(words_and_counts)
