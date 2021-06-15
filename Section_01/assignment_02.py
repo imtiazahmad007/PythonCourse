14bea9e5695bb1d198df906615ddb7d8bf35c998
royalshivu <66720997+royalshivu@users.noreply.github.com>
2021-06-15 22:43:36 +0530

    Update assignment_02.py

--- Section_01/assignment_02.py
+++ Section_01/assignment_02.py
@@ -5,7 +5,7 @@ We actually have 10 small, 12 large, and 12 medium boxes.
 Write code to correct this:
 
 
-print("We have {2} small boxes, {2} large boxes, {2} medium boxes".format(10,12,12))
+print("We have {0} small boxes, {1} large boxes, {2} medium boxes".format(10,12,12))
 
 """
 
